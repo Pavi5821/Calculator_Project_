594ee46489425a7a3b2239c48036c673b6e26fc8
Pavi5821 <pavithraraj538@gmail.com>
2025-08-13 10:54:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function addToDisplay(val) {
+    document.getElementById('display').value += val;
+  }
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  function calculate() {
+    try {
+      const result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch {
+      document.getElementById('display').value = 'Error';
+    }
+  }
